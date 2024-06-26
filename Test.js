622a498a412952210ba8009157e5769e10f6bc30
Efend1 <kujanglarang31@gmail.com>
2024-06-26 19:48:55 +0700

    push pertama menggunakan termux

---
+++ Test.js
@@ -0,0 +1,13 @@
+var url = {
+  protocol: 'https',
+  domain: 'www.github/Efend1.com',
+  path: '/',
+  
+  getUrl: function(){
+    return url.protocol + '://' + url.domain + url.path;
+  }
+   
+};
+
+url.getUrl();
+console.log(url);
